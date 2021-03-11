973c77022b73fffa32a0d65d2d5ed3966726b117
maureen <maureenannw@gmail.com>
2021-03-11 15:22:41 +0300

    add changes to JS

--- JS/scripts.js
+++ JS/scripts.js
@@ -27,5 +27,17 @@ function myFunction(){  //calling
      "Kwame",]
                //0       1        2          3           4         5           6
  var weekDays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
+   //validating
+   if(dd<=0||dd>31){
+    alert("invalid day please put the correct day!")
+}
+else if(mm<=0||mm>12){
+    alert("invalid year please put the correct month!")
+}
+if(yy<190||yy>2020){
+    alert("invalid year please put the correct year!")
+}
+else if(cc<19||cc>20){
+    alert("invalid century please put put the correct century")
+}    var day =new Date(cc + "-" + yy + "-" + mm + "-" + dd);    var dayWanted=day.getDay();
 }
