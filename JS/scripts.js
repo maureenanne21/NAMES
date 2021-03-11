4e61a6ebf803a4953230138354fae6f7dd618db1
maureen <maureenannw@gmail.com>
2021-03-11 15:03:25 +0300

    add changes to JS

---
+++ JS/scripts.js
@@ -0,0 +1,7 @@
+function myFunction(){  //calling
+    var cc=document.getElementById("century");
+    var yy=parseInt(document.getElementById("year").value)
+    var mm=parseInt(document.getElementById("month").value)
+    var dd=parseInt(document.getElementById("day").value)
+    var male=document.getElementById("male");
+    var female=document.getElementById("female");
